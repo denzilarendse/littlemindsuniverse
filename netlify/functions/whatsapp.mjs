d2414ef9034abe4630baff125210c421703ca2ed
littlemindsuniverse-za <denzilarendse30@gmail.com>
2026-09-26 08:22:34 +0200

    chore: remove WhatsApp Netlify adapter

--- netlify/functions/whatsapp.mjs
+++
@@ -1,4 +0,0 @@
-import handler from '../../api/whatsapp.js';
-import { adaptVercelHandler } from './_adapter.mjs';
-export default adaptVercelHandler(handler);
-export const config = { path: '/api/whatsapp' };
